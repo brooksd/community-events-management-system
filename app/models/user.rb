edbcbc9651a136de60c7718b447c1a6e464ce0a5
Donald Brooks Arum <brooksd079@gmail.com>
2022-09-07 21:13:45 +0300

    user model added and given appropriate relationships and validation

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ActiveRecord::Base
+    has_secure_password
+    has_many :events
+    validates :email, uniqueness: true
+    validates :name, :event_description, :email, :password, presence: true
+end
