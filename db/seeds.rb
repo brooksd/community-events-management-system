5b2b51a164fe8b877bf6e4dbecc90c82f6559a37
Donald Brooks Arum <brooksd079@gmail.com>
2022-09-08 19:14:42 +0300

    minor changes to the schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
-puts "🌱 Seeding spices..."
+puts "🌱 Seeding Greatness!..."
 
 # Seed your database here
+
+Review.create(name: "Donald Brooks", review: "the event was amazing!")
 
 puts "✅ Done seeding!"
